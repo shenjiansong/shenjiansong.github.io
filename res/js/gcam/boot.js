19254e6089763aefde3ca95d08ca7d2b9cd9140a
jayshen2015 <jayshen1986@gmail.com>
2024-02-02 18:28:54 +0800

    优化

--- res/js/gcam/boot.js
+++ res/js/gcam/boot.js
@@ -30,7 +30,7 @@ var X=window.X||{
 	debugger:true,
 	VERSION:2306,
 	GVERSION:function(){try{ return AZ.getVersion().split("_")[0]; }catch(e){ return "8.8"; }}(),
-	CACHE_VERSION:2.39,
+	CACHE_VERSION:2.40,
 	RES_CSS_VERSION:'1.0.0-9',
 	RES_JS_VERSION:'1.0.0-9',
 	RES_CUSTOM_VERSION:'1.0.0-9',
