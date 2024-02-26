83eb09d38391f4be2cdb3d55161bff7d5633016d
jayshen2015 <jayshen1986@gmail.com>
2024-02-26 10:17:45 +0800

    优化

--- res/js/util/D.js
+++ res/js/util/D.js
@@ -1,4 +1,4 @@
-layui.define([], function (exports) {
+layui.define(['jquery'], function (exports) {
 	var D={
 			header:{
 				welcome:'.',
@@ -65,13 +65,46 @@ layui.define([], function (exports) {
 				{{#  if(d.wx){ }}<div class="qrcode"><img width="160px" src="{{d.wx}}"></div>{{# } }}
 				  <div class="practice-mode">
 					<div class="text">
-					  <h4 class="title">我的联系方式</h4>
+					  <!--h4 class="title">如何联系</h4--!>
 					  {{#  if(d.mobile){ }}<p>手机<span class="iphone">{{d.mobile}}</span></p>{{# } }}
 					  {{#  if(d.qq){ }}<p>Q&nbsp;Q<span class="email">{{d.qq}}</span></p>{{# } }}
-					  {{#  if(d.email){ }}<p>邮箱<span class="email">{{d.email}}</span></p>{{# } }}
+					  {{#  if(d.email){ }}<p>与我联系<span class="email">{{d.email}}</span></p>{{# } }}
+					  <p><a  href="javascript:layui.D.privacy.show()">隐私策略<span class="txt">点击查看</span></a></p>
 					</div>
 				  </div>
 				</div></div>`
+		},
+		privacy:{
+			dom:null,
+			tempHtml:`<div class="privacy"><div class='text'>	<h1>隐私政策</h1><hr />
+<pre>
+  1：信息收集：我们可能会收集您的个人信息，包括但不限于姓名、电子邮件地址、IP 地址等，用于提供更好的服务和用户体验。
+
+  2：信息使用：您的个人信息可能会用于个性化广告投放、改善我们的产品和服务、以及与您沟通。我们不会出售或泄露您的个人信息。
+
+  3：Cookie 和跟踪技术：我们的网站可能会使用 cookie 和其他跟踪技术来收集和存储您的信息，以提供个性化的广告和改善用户体验。
+
+  4：第三方披露：在法律要求或为了保护我们的合法权益时，我们可能会与第三方分享您的个人信息。
+
+  5：数据安全：我们会采取合理的安全措施来保护您的个人信息，防止未经授权的访问、使用或泄露。
+
+  6：隐私权政策变更：我们可能会不时地更新我们的隐私权政策，更新后的政策将在本页面公布。
+
+  7：同意条款：通过使用本网站，即表示您同意本隐私政策的条款和条件。
+</pre></div>
+<div class='buttons'><input type="button" onclick="layui.D.privacy.hide()" value="关闭"></div>
+</div>`,
+			show:function(){
+				if(!this.dom){
+					this.dom=layui.$(this.tempHtml);
+					layui.$(document.body).append(this.dom);
+				}else{
+					this.dom.show();
+				}
+			},
+			hide:function(){
+				this.dom.hide();
+			}
 		}
 	}
 	exports('D', D)
